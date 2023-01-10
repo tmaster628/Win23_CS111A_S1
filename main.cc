e009577432113b44b3ae356847c3d7b791ff4df8
Trip <tmaster@myth.stanford.edu>
2023-01-10 11:14:38 -0800

    clear out starter code

--- main.cc
+++ main.cc
@@ -16,12 +16,8 @@ using namespace std;
  * 
 */
 void reverseVec(vector<int> &vec) {
-    for (int i = 0; i < vec.size()/2; i++) {
-        int temp = vec[i];
-        vec[i] = vec[vec.size() - 1 - i]; 
-        vec[vec.size() - 1 - i] = temp;
-    }
-    //(void)vec; // TODO: Write code here!
+    // TODO: Implement me!
+    (void)vec; 
 }
 
 /**
@@ -35,20 +31,8 @@ void reverseVec(vector<int> &vec) {
 */
 
 string aurify_cpp(string msg) {
-    //(void) msg; //TODO: Write code here!
-    //return "";
-
-    string toRet = "";
-    for (int i = 0; i < msg.size(); i++) {
-        if (msg[i] == 'o') {
-            toRet += "aur";
-        } else if (msg[i] == 'O') {
-            toRet += "AUR";
-        } else {
-            toRet += msg[i];
-        }
-    }
-    return toRet;
+    // TODO: Implement me!
+    return msg;
 }
 
 /**
@@ -62,21 +46,13 @@ string aurify_cpp(string msg) {
  * Note that the path is a char * and the tokens use strings.
  * 
  * Bonus points if you use c-string operations to tokenize the path.
+ * (This is because in a C environment, you can't use strings, but in a
+ * CPP environment, you can still use C-strings).
 */
 
 void get_path (const char *path, vector<string>& tokens) {
-
-    char buf[strlen(path) + 1];
-    strcpy(buf, path);
-    char *token = strtok(buf, "/");
-    tokens.push_back(string(token));
-
-    while (true) {
-        // fix this line.
-        char *token = strtok(NULL, "/");
-        if (!token) break;
-        tokens.push_back(string(token));
-    }
+    // TODO: Implement me!
+    (void) path, tokens;
 }
 
 /**
@@ -96,9 +72,31 @@ struct course {
 };
 
 void increment_units (vector<course> &all_cs_courses) {
-    for (course& c : all_cs_courses) {
+    for (course c : all_cs_courses) {
         c.unit_count++;
     }
+}
+
+/**
+ * Function #5:
+ * 
+ * After fixing their bug, the CS department has hired you for more jobs. This time,
+ * they want you correct the ALL_CS_COURSES vector to include accurate COURSE_RATING
+ * values. Currently, the COURSE_RATING value for every class in the vector is 0.0.
+ * 
+ * Luckily, you're given a map called RATINGS that maps string course codes to double 
+ * course ratings.
+ * 
+ * Your task is to ensure that the vector ALL_CS_COURSES has the correct ratings for 
+ * every course.
+ * 
+ * ex. all_cs_course: { "troccoli", "CS111", 5, 0.0 } -> { "troccoli", "CS111", 5, 5.0 }
+*/
+
+
+void change_rating (vector<course> &all_cs_courses, const map<string, double> &ratings) {
+    // TODO: Implement me!
+    (void) all_cs_courses, ratings;
 }
 
 
@@ -108,7 +106,7 @@ void increment_units (vector<course> &all_cs_courses) {
 int main() {
 
     vector<int> tests;
-    tests.resize(4);
+    tests.resize(5);
 
     // Running main just runs the test harness. You'll
     // Want to implement the functions below.
@@ -126,6 +124,8 @@ int main() {
         }
     }
     if (tests[0] == 0) {
+        cout << "Function #1: Passes the sample test!" << endl;
+    } else {
         cout << "Function #1: Passes the sample test!" << endl;
     }
 
@@ -146,6 +146,8 @@ int main() {
 
     if (tests[1] == 0) {
         cout << "Function #2: Passes the sample test!" << endl;
+    } else {
+        cout << "Function #2: FAILS the sample test!" << endl;
     }
 
     cout << "#################################\n\n" << endl;
@@ -157,17 +159,26 @@ int main() {
     vector<string> sol = { "Desktop", "cs111", "assignment1" };
     const char * path_literal = "Desktop/cs111/assignment1";
     get_path(path_literal, path);
-    for (int i = 0; i < path.size(); i++)  {
-        if (sol[i] != path[i]) {
-            cout << "Sample mismatch at index " << i << endl;
-            cout << "Expected Output: " << sol[i] << endl;
-            cout << "Actual Output: " << path[i] << endl; 
-            tests[2] = 1;
+    if (sol.size() != path.size()) {
+        cout << "Path output and solution output do not match!" << endl;
+        cout << "Expected output size: 3" << endl;
+        cout << "Actual output size: " << path.size() << endl;
+        tests[2] = 1;
+    } else {
+        for (int i = 0; i < sol.size(); i++)  {
+            if (sol[i] != path[i]) {
+                cout << "Sample mismatch at index " << i << endl;
+                cout << "Expected Output: " << sol[i] << endl;
+                cout << "Actual Output: " << path[i] << endl; 
+                tests[2] = 1;
+            }
         }
     }
 
     if (tests[2] == 0) {
         cout << "Function #3: Passes the sample test!" << endl;
+    } else {
+        cout << "Function #3: FAILS the sample test!" << endl;
     }
 
     cout << "#################################\n\n" << endl;
@@ -176,10 +187,10 @@ int main() {
     cout << "Testing Function #4: Unit count increment" << endl;
     cout << "#################################" << endl;
 
-    course c1 = {"","",1, 2.0};
-    course c2 = {"","",4, 2.0};
-    course c3 = {"","",2, 2.0};
-    course c4 = {"","",3, 2.0};
+    course c1 = {"Troccoli","CS111",1, 0.0};
+    course c2 = {"Gregg","CS107",4, 0.0};
+    course c3 = {"Schwarz","CS106B",2, 0.0};
+    course c4 = {"Parlante","CS106A",3, 0.0};
 
     vector<course> cs_courses = { c1, c2, c3, c4 };
     auto dup = cs_courses;
@@ -190,15 +201,54 @@ int main() {
             cout << "Input: " << dup[i].unit_count << endl;
             cout << "Expected Output: " << (dup[i].unit_count + 1) << endl;
             cout << "Actual Output: " << cs_courses[i].unit_count << endl; 
-            tests[3] == 1;
+            tests[3] = 1;
         }
     }
 
     if (tests[3] == 0) {
         cout << "Function #4: Passes the sample test!" << endl;
+    } else {
+        cout << "Function #4: FAILS the sample test!" << endl;
     }
 
     cout << "#################################\n\n" << endl;
+
+    cout << "Testing Function #5: Updating Course Mappings" << endl;
+    cout << "#################################" << endl;
+
+    course c1_2 = {"Troccoli","CS111",5, 0.0};
+    course c2_2 = {"Gregg","CS107",5, 0.0};
+    course c3_2 = {"Schwarz","CS106B",5, 0.0};
+    course c4_2 = {"Parlante","CS106A",5, 0.0};
+
+    vector<course> cs_courses_ = { c4_2, c3_2, c2_2, c1_2 };// Don't ask.
+
+    map<string, double> course_ratings = { {"CS106A", 4.5 }, {"CS106B", 4.5}, {"CS107", 4.7}, {"CS111", 5.0}};
+    auto elem = course_ratings.begin();
+
+    change_rating (cs_courses_, course_ratings);
+    for (int i = 0; i < course_ratings.size(); i++) {
+        if (cs_courses_[i].course_rating != (*elem).second) {
+            cout << "Sample mismatch!" << endl;
+            cout << "Expected Output: " << (*elem).first << ": "<<(*elem).second << endl;
+            cout << "Actual Output: " << cs_courses_[i].code  << ": "<< cs_courses_[i].course_rating << endl; 
+            tests[4] = 1;
+        }
+        elem++;
+    }
+
+    if (tests[4] == 0) {
+        cout << "Function #5: Passes the sample test!" << endl;
+    } else {
+        cout << "Function #5: FAILS the sample test" << endl;
+    }
+
+    cout << "#################################\n\n" << endl;
+
+    if (!tests[0] && !tests[1] && !tests[2] && !tests[3] && !tests[4]) {
+        cout << "You passed all of the tests. Nice!" << endl;
+    }
+
 }
 
 
